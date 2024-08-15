4ae2ef5b5658e8824190deb55b39d358fab20d05
Yaroslav Petrov <PersonalYP00@gmail.com>
2024-08-16 02:46:10 +0300

    commit changes

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -13,15 +13,15 @@ export class Header extends DivComponent {
         this.el.classList.add('header');
         this.el.innerHTML = `
             <div>
-                <img src="../static/movie-logo.svg" alt="logo" id="main-logo"/>
+                <img src="static/movie-logo.svg" alt="logo" id="main-logo"/>
             </div>
             <div class="menu">
                 <a href="#" class="menu__item">
-                    <img src="../static/search.svg" alt="search-icon" />
+                    <img src="static/search.svg" alt="search-icon" />
                     Search
                 </a>
                 <a href="#favourites" class="menu__item">
-                    <img src="../static/favorites.svg" alt="favourites-icon" />
+                    <img src="static/favorites.svg" alt="favourites-icon" />
                     Favourites
                     <div class="menu__counter">
                         ${this.appState.favourites.length}
