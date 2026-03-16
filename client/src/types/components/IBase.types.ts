6a8d342cdb81c8505a984da3758e6ed501c7de3d
YP00 <personalYP00@icloud.com>
2026-03-16 14:11:07 +0200

    feat: button style adjustments

--- client/src/types/components/IBase.types.ts
+++ client/src/types/components/IBase.types.ts
@@ -2,5 +2,5 @@ export interface IButton {
   type?: 'button' | 'submit' | 'reset'
   disabled?: boolean
   size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl' | '4xl'
-  variant?: 'primary' | 'secondary' | 'tertiary'
+  variant?: 'primary' | 'secondary' | 'tertiary' | 'tab'
 }
