8c79606e2c24565da59c39ad47697d1e2c177a28
YP00 <personalYP00@icloud.com>
2026-03-16 12:26:01 +0200

    feat: BaseButton structure

--- client/src/components/base/BaseButton.vue
+++ client/src/components/base/BaseButton.vue
@@ -1,7 +1,40 @@
 <script setup lang="ts">
+import type {
+  IButton
+} from '@/types'
 
+const {
+  type = 'button',
+  disabled = false,
+  size = 'md',
+  variant = 'primary'
+} = defineProps<IButton>()
+
+const emit = defineEmits<{
+  (e: 'click', event: PointerEvent): void
+}>()
+
+  // Init
+  // Constants
+  // Methods
+function handleClick(e: PointerEvent): void {
+  if (disabled) {
+    return
+  }
+  return emit('click', e)
+}
 </script>
 
 <template>
-  <button />
+  <button
+    :type="type"
+    :disabled="disabled"
+    :class="['baseButton', size, variant]"
+
+    @click="handleClick"
+  >
+    <slot name="left" />
+    <slot />
+    <slot name="right" />
+  </button>
 </template>
---
+++ client/src/types/components/IBase.types.ts
@@ -0,0 +1,6 @@
+export interface IButton {
+  type?: 'button' | 'submit' | 'reset'
+  disabled?: boolean
+  size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl' | '4xl'
+  variant?: 'primary' | 'secondary' | 'tertiary'
+}
