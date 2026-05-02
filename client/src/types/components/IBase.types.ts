ff02fe2b7a9c6be8902f625c74462a34a7c63587
YP00 <personalYP00@icloud.com>
2026-05-03 02:14:34 +0300

    feat: BaseButton styling

--- client/src/types/components/IBase.types.ts
+++ client/src/types/components/IBase.types.ts
@@ -2,5 +2,5 @@ export interface IBaseButton {
   type?: 'button' | 'submit' | 'reset'
   disabled?: boolean
   size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl'
-  variant?: 'primary' | 'secondary' | 'tertiary' | 'tab'
+  variant?: 'primary' | 'secondary' | 'tertiary'
 }
