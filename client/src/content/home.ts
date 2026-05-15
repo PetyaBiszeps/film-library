6feb687e58970a2589b227bea72a2370db1d4207
YP00 <personalYP00@icloud.com>
2026-05-15 03:13:46 +0300

    feat: add header to home/

---
+++ client/src/content/home.ts
@@ -0,0 +1,7 @@
+export default {
+  header: {
+    title: 'Home',
+    description: 'Find films for tonight, keep your watchlist close.'
+  },
+  main: {}
+} as const
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,13 +1,21 @@
 <script setup lang="ts">
 import CommonSearch from '@/components/common/CommonSearch.vue'
+import HOME_CONTENT from '@/content/home.ts'
 </script>
 
 <template>
   <section class="home">
-    Page name
+    <header class="home__header">
+      <div class="home__header__text">
+        <h1>{{ HOME_CONTENT.header.title }}</h1>
+        <p>{{ HOME_CONTENT.header.description }}</p>
+      </div>
 
-    <CommonSearch />
+      <CommonSearch />
+    </header>
 
-    Page content here!
+    <main class="home__main">
+      Page content here!
+    </main>
   </section>
 </template>
